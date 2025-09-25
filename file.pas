cd8b8e1da773dafaf57a515f7e7921950cd98481
irayanikalilaharahap-web <irayanikalilaharahap@gmail.com>
2025-09-25 15:35:40 +0700

    pertemuan 2 : operator

---
+++ file.pas
@@ -0,0 +1,48 @@
+uses crt;
+var 
+    panjang,lebar : integer;
+
+begin
+    clrscr;
+    // writeln ('hello world');
+    p := 5;
+    l := 2;
+
+    luas := p * l;
+    keliling := 2 * p + l;
+    // kali := a * b;
+    // bagi := a / b;
+    // modulo := a mod b;
+
+    writeln('hasil l  :', l);
+    writeln('hasil k :', k);
+    // writeln('hasil kali :', kali);
+    // writeln('hasil bagi :', bagi:0:2); //4.00
+    // writeln('hasil mod :', modulo);
+
+    // writeln(a = b);
+    // writeln(a <> b);
+    // writeln(a < b);
+    // writeln(a > b);
+    // writeln(a <= b);
+    // writeln(a >= b);
+
+    // writeln(true and true);
+    // writeln(true and false);
+    // writeln(false and true);
+    // writeln(false and false);
+
+    // writeln(true or true);
+    // writeln(true or false);
+    // writeln(false or true);
+    // writeln(false or false);
+
+    // writeln(not true);
+    // writeln(not false);
+
+    // luas lapangan := panjang * lebar;
+    // keliling lapangan := 2 * panjang + lebar;
+
+    // writeln('hasil luas lapangan :', luas lapangan);
+    // writeln('hasil keliling lapangan :', keliling lapangan);
+end.
