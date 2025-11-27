768358332e53d2940fe718904676264733a482e7
irayanikalilaharahap-web <irayanikalilaharahap@gmail.com>
2025-11-27 10:45:33 +0700

    pertemuan 4 : perulangan

---
+++ perloncatan.pas
@@ -0,0 +1,14 @@
+uses crt;
+label a, b, c, d, e;
+
+begin 
+    a:writeln('hello world'); 
+    goto b;
+    d:writeln('Fasilkom -TI');
+    goto e;
+    c:writeln('Ilmu komputer');
+    goto d;
+    b:writeln('IKLC');
+    goto c;
+    e:
+end.
---
+++ perulangan.pas
@@ -0,0 +1,33 @@
+uses crt; 
+var 
+i,j : integer; 
+pasword : string;
+begin
+clrscr;
+
+    // for i := 1 to 5 do 
+    //     writeln('hello world');
+
+    // for i := 1 to 5 do 
+    //     begin 
+    //         for i := 1 to i do
+    //             write('* ');
+    //         writeln;
+    //     end;
+
+    // while
+    // i := 1; 
+    // while i <= 10 do 
+    //     begin 
+    //         if i mod 2 = 0 then 
+    //             write(i, ' ');
+    //         i := i + 1;
+    //     end;
+
+    // repeat
+    // repeat
+    //     write('masukkan pasword anda : '); readln 
+    //     (pasword);
+    // until (pasword = '150207');
+    // writeln('berhasil');
+end.
